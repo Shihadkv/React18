e47b5b7fad67d748287e0ceb2f21c128354633cb
Shihadkv <shihad530@gmail.com>
2023-09-05 17:40:09 +0530

    functional components-1

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-const heading= React.createElement('h1', {id: "heading"},"Hi welcome to  shops");
+// React element we already discussed
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// JSX html like syntax - it is transpiled to JS engine by PARCEL
+const jsxHeading= <h1 id = "heading" className="head" > Its a jsx example</h1>
+console.log(jsxHeading)
 
-root.render(heading);
-console.log(heading)
+
+
+// React component
+// 1. class Based - old way
+// 2. Functional Component - new way
+
+const Title = () => <h1>Its a functional component of title</h1>
+let number = 13055;
+let non = <h1>hi all</h1>
+const DataComponent = () => (
+    <div> 
+         {non}
+         <h2>{number}, {non}</h2>
+         <Title/>
+         <h1>Its a functional component jsx</h1>
+    </div>
+    )
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<DataComponent/>)
