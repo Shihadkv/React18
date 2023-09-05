068391fc2b6a3c6a6dd79149e9d63fc0d657aa33
Shihadkv <shihad530@gmail.com>
2023-09-05 13:30:21 +0530

    react day - 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
 const heading= React.createElement('h1', {id: "heading"},"Hi welcome to  shops");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # React18
+
+# Parcel
+ - Dev build
+ - Local server
+ - Hot Module Replacement (HMR)
+ - its a file tracking algorithm in cpp
+ - Faster building by caching
+ - better error handling 
+ - Can use HTTPS
+
